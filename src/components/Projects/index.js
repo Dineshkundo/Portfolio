28be3867402b87d3c83dfae2f05bff0d5197e873
Dinesh Kundo <kundodinesh0@gmail.com>
2024-02-04 21:53:43 +0530

    first commit

---
+++ src/components/Projects/index.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { useState } from 'react'
+import { Container, Wrapper, Title, Desc, CardContainer, ToggleButtonGroup, ToggleButton, Divider } from './ProjectsStyle'
+import ProjectCard from '../Cards/ProjectCards'
+import { projects } from '../../data/constants'
+
+
+const Projects = ({openModal,setOpenModal}) => {
+  const [toggle, setToggle] = useState('all');
+  return (
+    <Container id="projects">
+      <Wrapper>
+        <Title>Projects</Title>
+        <Desc>
+          I have worked on a wide range of projects.Here are some of my projects.
+        </Desc>
+        <ToggleButtonGroup >
+          {toggle === 'all' ?
+            <ToggleButton active value="all" onClick={() => setToggle('all')}>All</ToggleButton>
+            :
+            <ToggleButton value="all" onClick={() => setToggle('all')}>All</ToggleButton>
+          }
+          <Divider />
+          {toggle === 'Cloud' ?
+            <ToggleButton active value="Cloud" onClick={() => setToggle('Cloud')}>Cloud</ToggleButton>
+            :
+            <ToggleButton value="Cloud" onClick={() => setToggle('Cloud')}>Cloud</ToggleButton>
+          }
+          <Divider />
+          {toggle === 'Devops' ?
+            <ToggleButton active value="Devops" onClick={() => setToggle('Devops')}>Devops</ToggleButton>
+            :
+            <ToggleButton value="Devops" onClick={() => setToggle('Devops')}>Devops</ToggleButton>
+          }
+          {/* <Divider />
+          {toggle === 'machine learning' ?
+            <ToggleButton active value="machine learning" onClick={() => setToggle('machine learning')}>MACHINE LEARNING</ToggleButton>
+            :
+            <ToggleButton value="machine learning" onClick={() => setToggle('machine learning')}>MACHINE LEARNING</ToggleButton>
+          } */}
+        </ToggleButtonGroup>
+        <CardContainer>
+          {toggle === 'all' && projects
+            .map((project,e) => (
+              <ProjectCard key={e} project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+          {projects
+            .filter((item) => item.category === toggle)
+            .map((project) => (
+              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+        </CardContainer>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Projects
---
+++ src/data/constants.js
@@ -0,0 +1,306 @@
+export const Bio = {
+  name: "Dinesh Kundo",
+  roles: [
+    "AWS Cloud Developer",
+    "DevOps DEveloper",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/Dineshkundo",
+  resume:
+    "https://drive.google.com/file/d/1K_VNaEVWYXors4iN_PhbLkdDQas_gbRe/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/dinesh-kundo/",
+  twitter: "https://twitter.com/KundoDinesh",
+  // insta: "https://www.instagram.com/rishav_chanda/",
+  // facebook: "https://www.facebook.com/rishav.chanda.165/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      // {
+      //   name: "Redux",
+      //   image:
+      //     "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      // },
+      // {
+      //   name: "Next Js",
+      //   image:
+      //     "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      // },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      // {
+      //   name: "Material UI",
+      //   image:
+      // },
+      // {
+      //   name: "Flutter",
+      //   image:
+      //     "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+      // },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "RDS",
+        image: "https://seeklogo.com/images/A/aws-dynamodb-logo-CF7BCC577D-seeklogo.com.png",
+      },
+      {
+        name: "Redshift",
+        image: "https://seeklogo.com/images/A/aws-redshift-logo-B050BB6A39-seeklogo.com.png",
+      },
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:" https://www.pngfind.com/pngs/m/136-1363736_express-js-icon-png-transparent-png.png"
+      },
+      // {
+      //   name: "Graph Ql",
+      //   image: "https://www.vectorlogo.zone/logos/graphql/graphql-icon.svg",
+      // },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Postgresql",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      
+    ],
+  },
+  {
+    title: "Cloud & DevOps",
+    skills: [
+      {
+        name: "AWS",
+        image:
+          "https://www.vectorlogo.zone/logos/amazon_aws/amazon_aws-icon.svg",
+      },
+      {
+        name: "Terraform",
+        image:
+          "https://www.vectorlogo.zone/logos/terraformio/terraformio-icon.svg",
+      },
+      {
+        name: "Jenkins",
+        image:
+          "https://www.vectorlogo.zone/logos/jenkins/jenkins-icon.svg",
+      },
+      
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name:"Linux",
+        image:
+          "https://www.vectorlogo.zone/logos/linux/linux-icon.svg"
+      },
+      {
+        name: "Github Actions",
+        image:
+          "https://seeklogo.com/images/G/github-actions-logo-031704BDC6-seeklogo.com.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://www.vectorlogo.zone/logos/github/github-icon.svg",
+      },
+      // {
+      //   name: "Docker",
+      //   image:
+      //     "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      // },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg",
+      },
+      // {
+      //   name: "Adobe XD",
+      //   image:
+      //     "https://camo.githubusercontent.com/c205ecbe12500177d102169d97bc1c17c545155fdf5ec78c08d54ac53e5b38c1/68747470733a2f2f63646e2e776f726c64766563746f726c6f676f2e636f6d2f6c6f676f732f61646f62652d78642e737667",
+      // },
+      // {
+      //   name: "Figma",
+      //   image:
+      //     "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+      // },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://avatars.githubusercontent.com/u/8909564?s=200&v=4",
+    role: "Software Engineering Intern",
+    company: "Osmosys Software Solution Pvt. Ltd.",
+    date: "Sep 2022 – Feb 2023 | Hyderabad, TG",
+    desc: "I worked under the IR-365 Project as an intern.",
+    skills: [
+      ".NET Framework",
+      "API Testing",
+      "Technical Support",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "SQL",
+      
+    ],
+    doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+  },
+  {
+    id: 0,
+    img: "https://d1u7xmwfjh1cnu.cloudfront.net/feed/content/logo-jpy0.png",
+    role: "Frontend Engineer Intern",
+    company: "Era Interface",
+    date: "May 2022 – Aug 2021 | Bangalore, Ka",
+    desc: "Working on the frontend of the Cloudlication using ReactJS, Redux, and Material UI.",
+    skills: [
+      "ReactJS",
+      "NodeJs",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    doc: "",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/GIET_University_Logo.tif/lossy-page1-883px-GIET_University_Logo.tif.jpg",
+    school: "Gandhi Institute Of Engineering And Technology, GUNUPUR",
+    date: "April 2019 - April 2023",
+    grade: "8.6 CGPA",
+    desc: "I completed my Bachelor's degree in Computer Science and Engineering.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://www.gpsgnp.edu.in/wp-content/uploads/2020/08/cropped-logo-3-1.png",
+    school: "Gandhi Public School, GUNUPUR",
+    date: "Apr 2017 - Apr 2019",
+    grade: "69%",
+    desc: "I completed my class 12 high school education at Gandhi Public School, Gunupur, where I studied Science with Computer Science.",
+    degree: "CBSE(XII)",
+  },
+  {
+    id: 2,
+    img: "https://www.gpsgnp.edu.in/wp-content/uploads/2020/08/cropped-logo-3-1.png",
+    school: "Gandhi Public School, GUNUPUR",
+    date: "Apr 2016 - Apr 2017",
+    grade: "8.2 CGPA",
+    desc: "I completed my class 10 education at Gandhi Public School, GUNUPUR",
+    degree: "CBSE(X)",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "CLOUD MIGRATION PROJECT",
+    date: "Dec 2023",
+    description:
+      "Migration of Infrastructure data and applications out of legacy on-premise data centers into the cloud",
+    image:
+      "https://www.knowledgehut.com/_next/image?url=https%3A%2F%2Fd2o2utebsixu4k.cloudfront.net%2Fmedia%2Fimages%2F81ba9660-249c-484e-b21e-a550611e3162.png&w=750&q=75",
+    tags: [
+      "AWS",
+      "AWS Application Migration Service",
+    ],
+    category: "Cloud",
+    github: "https://github.com/Dineshkundo/AWS-MIGRATION-PROJECTT",
+    webapp: "https://aquatic-tanker-344.notion.site/Cloud-Migration-9ee740e55c0f46c691e352a6ed22ca1f?pvs=4",
+  },
+  {
+    id: 9,
+    title: "AUTOMATE INFRASTRUCTURE ON AWS USING TERRAFORM",
+    date: "Dec 2023",
+    description:
+      "Migration of Infrastructure data and applications out of legacy on-premise data centers into the cloud",
+    image:
+      "https://private-user-images.githubusercontent.com/60452355/295123329-15af2a1c-c9f3-4425-91f8-01e7a24b900d.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDUyOTU2MjgsIm5iZiI6MTcwNTI5NTMyOCwicGF0aCI6Ii82MDQ1MjM1NS8yOTUxMjMzMjktMTVhZjJhMWMtYzlmMy00NDI1LTkxZjgtMDFlN2EyNGI5MDBkLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAxMTUlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMTE1VDA1MDg0OFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWMzN2MzNWU3ZTlhODBiNjVhZmUwNTkwMjA3ZmI0N2U0MDEzMzE3MzFmOGMxNmQyZDhiODFkNTczYTIzOGQ4ZDkmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.d_hqn2lYyLeYACczk6IcPXKYRV_BSEiOC04ahRJJCOs",
+    tags: [
+      "AWS",
+      "Terraform",
+    ],
+    category: "Cloud",
+    github: "https://github.com/Dineshkundo/AUTOMATE-INFRASTRUCTURE-ON-AWS-USING-TERRAFORM",
+    webapp: "",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
